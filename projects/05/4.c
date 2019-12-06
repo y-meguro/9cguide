6c07b02dca5ff9a609d9cae9ce1b5a9bf45cb8dc
Yohei Meguro <y.meguro.2008@gmail.com>
2019-12-06 17:08:25 +0900

    add 05

---
+++ projects/05/4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+
+    x = 10;
+    y = x * 10 + 20;
+    printf("%5d\n", y);
+
+    return 0;
+}
---
+++ projects/05/5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+
+    x = 1000 - (198 + 138 * 2) * 1.05;
+    printf("%d円\n", x);
+
+    return 0;
+}
